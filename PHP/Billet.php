7e33b21a0d4de61cabef2c7ec9a563dca4b7d62c
Thatonedragon <looijdylan@gmail.com>
2024-01-22 11:54:33 +0100

    Json reception fait

--- PHP/Billet.php
+++ PHP/Billet.php
@@ -1,42 +1,4 @@
-<?php
-// Process Form Data
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    // Assuming your form data is sent as JSON
-    $json = file_get_contents('php://input');
-    $formData = json_decode($json, true);
+<?php $str_json = file_get_contents('php://input');
+    echo $str_json;
 
-    // Perform validation if needed
-
-    // Connect to your database
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "tpmariadb";
-
-    $conn = new mysqli($servername, $username, $password, $dbname);
-
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-
-    // Insert data into the database
-    $sql = "INSERT INTO your_table (user_name, number_of_places, pass_type, days, user_message) 
-            VALUES (
-                '" . $conn->real_escape_string($formData['user_name']) . "',
-                '" . $conn->real_escape_string($formData['number_of_places']) . "',
-                '" . $conn->real_escape_string($formData['pass_type']) . "',
-                '" . $conn->real_escape_string(implode(",", $formData['days'])) . "',
-                '" . $conn->real_escape_string($formData['user_message']) . "'
-            )";
-
-    if ($conn->query($sql) === TRUE) {
-        echo json_encode(array("message" => "Data inserted successfully"));
-    } else {
-        echo json_encode(array("error" => "Error: " . $sql . "<br>" . $conn->error));
-    }
-
-    // Close the connection
-    $conn->close();
-}
 ?>
