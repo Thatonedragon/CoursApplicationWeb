9f5184d7f9baf634ea31f64b74d68a618003387c
Thatonedragon <looijdylan@gmail.com>
2024-01-22 12:00:26 +0100

    SaveState

--- PHP/Billet.php
+++ PHP/Billet.php
@@ -1,12 +1,15 @@
 <?php 
 
     $str_json = file_get_contents('php://input');
+    $Data = json_decode($str_json, true);
     echo $str_json;
 
     $host = '127.0.0.1:3307';
     $dbname = 'tpmariadb';
     $user = 'root';
     $pass = '';
+
+    $conn = new mysqli($host, $user, $pass, $dbname);
 
 
 ?>
