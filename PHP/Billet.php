4822ba56c62582d2ac90edc4cc066381b2d7f31b
Thatonedragon <looijdylan@gmail.com>
2024-01-22 11:54:51 +0100

    BOnjour

--- PHP/Billet.php
+++ PHP/Billet.php
@@ -1,4 +1,12 @@
-<?php $str_json = file_get_contents('php://input');
+<?php 
+
+    $str_json = file_get_contents('php://input');
     echo $str_json;
+
+    $host = '127.0.0.1:3307';
+    $dbname = 'tpmariadb';
+    $user = 'root';
+    $pass = '';
+
 
 ?>
