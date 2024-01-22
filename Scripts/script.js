b0d81024fa50eec1aca0a48f7e9b03dd7623fd15
Thatonedragon <looijdylan@gmail.com>
2024-01-22 08:28:32 +0100

    Mise a 100

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,5 +1,5 @@
 function Onstart(){ 
-    var random = Math.floor(Math.random() * 10);;
+    var random = Math.floor(Math.random() * 100);;
 
     document.getElementById('nbrvisiteur').innerHTML = "Il y a "+ random + " vistiteurs sur la site actuellement";
 
