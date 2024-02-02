1aaf667d870b4d0d98bab918621d5ad36d056b08
Thatonedragon <looijdylan@gmail.com>
2024-02-02 19:08:38 +0100

    Ajout Login

--- PHP/Billet.php
+++ PHP/Billet.php
@@ -19,32 +19,61 @@ try {
 
     $name = $Data['user_name'];
     $anotherField = $Data['type_pass'];
-
-    //les jours sont a faire, si plusieurs jours, plusieurs requettes,
-
-    $day = 'Vendredi';
-
-
-
     $number = $Data['nombre_de_place'];
     $status = $Data['user_message'];
+    //les jours sont a faire, si plusieurs jours, plusieurs requettes,
+    $day = $Data['jours'];
 
-    $sql = "INSERT INTO detail ( NOM_RESERVATION, TYPE, JOUR_RESERVATION, NB_PLACE, COMMENTAIRE) VALUES (:name, :anotherField, :day, :number, :status)";
 
-    $stmt = $pdo->prepare($sql);
+    if($day['vendredi']== true){
+        
+        $sql = "INSERT INTO detail ( NOM_RESERVATION, TYPE, JOUR_RESERVATION, NB_PLACE, COMMENTAIRE) VALUES (:name, :anotherField, '2024-05-10', :number, :status)";
 
-    $stmt->bindParam(':name', $name);
-    $stmt->bindParam(':anotherField', $anotherField);
-    $stmt->bindParam(':day', $day);
-    $stmt->bindParam(':number', $number);
-    $stmt->bindParam(':status', $status);
+        $stmt = $pdo->prepare($sql);
 
-    $stmt->execute();
+        $stmt->bindParam(':name', $name);
+        $stmt->bindParam(':anotherField', $anotherField);
+        $stmt->bindParam(':number', $number);
+        $stmt->bindParam(':status', $status);
+
+        $stmt->execute();
+    }
+
+    if($day['samedi']== true){
+        
+        $sql = "INSERT INTO detail ( NOM_RESERVATION, TYPE, JOUR_RESERVATION, NB_PLACE, COMMENTAIRE) VALUES (:name, :anotherField, '2024-05-11', :number, :status)";
+
+        $stmt = $pdo->prepare($sql);
+
+        $stmt->bindParam(':name', $name);
+        $stmt->bindParam(':anotherField', $anotherField);
+        $stmt->bindParam(':number', $number);
+        $stmt->bindParam(':status', $status);
+
+        $stmt->execute();
+    }
+    if($day['dimanche']== true){
+        
+        $sql = "INSERT INTO detail ( NOM_RESERVATION, TYPE, JOUR_RESERVATION, NB_PLACE, COMMENTAIRE) VALUES (:name, :anotherField, '2024-05-12', :number, :status)";
+
+        $stmt = $pdo->prepare($sql);
+
+        $stmt->bindParam(':name', $name);
+        $stmt->bindParam(':anotherField', $anotherField);
+        $stmt->bindParam(':number', $number);
+        $stmt->bindParam(':status', $status);
+
+        $stmt->execute();
+    }
+
+
+
+    
+
 
     echo "Record inserted successfully";
 } catch(PDOException $e) {
     echo "Error: " . $e->getMessage();
-
 }
 
 $pdo = null;
